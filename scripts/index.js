bddbb5ba0a6dd22f87e936d1de3bf42d2cc0b46c
proofblame <proofblame@gmail.com>
2020-10-17 00:40:12 +0300

    js

--- scripts/index.js
+++ scripts/index.js
@@ -1,48 +1,54 @@
-const profileEditButton = document.querySelector('.profile__edit-button');
-const profileAddButton = document.querySelector('.profile__add-button');
-const popupCloseButton = document.querySelector('.popup__close-button');
-const formElement = document.querySelector('.popup__form');
-const nameInput = document.querySelector('.popup__input_name');
-const jobInput = document.querySelector('.popup__input_job');
-const profileAuthor = document.querySelector('.profile__author');
-const profileAuthorStatus = document.querySelector('.profile__author-status');
-const elementsList = document.querySelector('.elements__list');
-const editPopup = document.querySelector('.popup_type_popup-edit-profile');
-const addCard = document.querySelector('.popup_type_popup-add-card');
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileAddButton = document.querySelector(".profile__add-button");
+const popupCloseButton = document.querySelector(".popup__close-button");
+const formElement = document.querySelector(".popup__form");
+const nameInput = document.querySelector(".popup__input_name");
+const jobInput = document.querySelector(".popup__input_job");
+const profileAuthor = document.querySelector(".profile__author");
+const profileAuthorStatus = document.querySelector(".profile__author-status");
+const elementsList = document.querySelector(".elements__list");
+const editPopup = document.querySelector(".popup_type_popup-edit-profile");
+const addCard = document.querySelector(".popup_type_popup-add-card");
 
 // Массив с карточками
 const initialCards = [
     {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+        name: "Архыз",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     },
     {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        name: "Челябинская область",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
     },
     {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+        name: "Иваново",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
     },
     {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+        name: "Камчатка",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
     },
     {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        name: "Холмогорский район",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
     },
     {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-]; 
-
+        name: "Байкал",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    },
+];
 
 // Добавляем карточки на страницу
 const renderCards = () => {
-    const card = initialCards.map(element => {
-        return `<li class="elements__item">
+    const card = initialCards
+        .map((element) => {
+            return `<li class="elements__item">
                     <figure class="element">
                         <img src="${element.link}" alt="${element.name}" class="element__image">
                         <div class="element__body">
@@ -52,9 +58,10 @@ const renderCards = () => {
                         <button class="element__delete-button buttons" type="button"></button>
                     </figure>
                 </li>`;
-    }).join('');
+        })
+        .join("");
 
-    elementsList.insertAdjacentHTML('afterbegin', card);
+    elementsList.insertAdjacentHTML("afterbegin", card);
 };
 renderCards();
 
@@ -62,11 +69,11 @@ renderCards();
 // через модификатор popup_opened
 
 function popupToggle() {
-    editPopup.classList.toggle('popup_opened');
+    editPopup.classList.toggle("popup_opened"); 
     if (editPopup.classList.contains("popup_opened")) {
         nameInput.value = profileAuthor.textContent;
         jobInput.value = profileAuthorStatus.textContent;
-    }
+    };
 }
 
 // Создаем событие указателя
@@ -90,10 +97,13 @@ function formSubmitHandler(evt) {
 formElement.addEventListener("submit", formSubmitHandler);
 
 // Слушатель событий для кнопки редактирования профиля
-profileEditButton.addEventListener("click", popupToggle);
+profileEditButton.addEventListener("click", () => popupToggle());
 
 // Слушатель событий для кнопки закрытия попапа
-popupCloseButton.addEventListener("click", popupToggle); 
+popupCloseButton.addEventListener("click", () => popupToggle());
 
 // Слушатель событий попапа (его закрытие) при клике на фоне
 editPopup.addEventListener("click", onClicPopupBackgroundkListener);
+
+// Слушатель событий для кнопки добавления карточки
+profileAddButton.addEventListener("click", () => popupToggle());
