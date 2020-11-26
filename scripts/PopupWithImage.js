8b9b745c576037b7b90ec6976850109f5654454e
Timur Marokko <proofblame@gmail.com>
2020-11-26 22:35:52 +0300

    refactor: Удалить неиспользуемые файлы, справить функцию в PopupWithImage

--- scripts/PopupWithImage.js
+++ scripts/PopupWithImage.js
@@ -1,7 +1,7 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
-    open = (name, link) => {
+    open (name, link) {
         this._popup.querySelector(".popup__image").src = link;
         this._popup.querySelector(".popup__image").alt = name;
         this._popup.querySelector(".popup__title").textContent = name;
