1647f03ff4ac3b1f399370c48bc38a5b47280e55
Timur Marokko <proofblame@gmail.com>
2020-11-26 22:38:57 +0300

    refactor: Исходное состояние перед установкой Webpack

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ import {
     settings,
     initialCards,
     profileSelector,
-} from "./constants.js";
+} from "./scripts/constants.js";
 
-import { Card } from "./Card.js";
-import { FormValidator } from "./FormValidator.js";
+import { Card } from "./scripts/Card.js";
+import { FormValidator } from "./scripts/FormValidator.js";
 
-import Section from "./Section.js";
-import PopupWithImage from "./PopupWithImage.js";
-import PopupWithForm from "./PopupWithForm.js";
-import UserInfo from "./UserInfo.js";
+import Section from "./scripts/Section.js";
+import PopupWithImage from "./scripts/PopupWithImage.js";
+import PopupWithForm from "./scripts/PopupWithForm.js";
+import UserInfo from "./scripts/UserInfo.js";
 
 // Рендер карточек из массива
 // Создание экземпляра класса отображения карточек на странице
