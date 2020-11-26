b443110543a4c4f61d0ec76f2650caceb7a03bcd
Timur Marokko <proofblame@gmail.com>
2020-11-26 20:33:18 +0300

    refactor: Добавить закрытие попапа по оверлею

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -6,6 +6,7 @@ export default class Popup {
     open() {
         this._popup.classList.add("popup_opened");
         document.addEventListener("keydown", this._handleEscClose.bind(this));
+        document.addEventListener("click", this._handleClickOnOverlay.bind(this));
     }
 
     close() {
@@ -14,18 +15,26 @@ export default class Popup {
             "keydown",
             this._handleEscClose.bind(this)
         );
+        document.removeEventListener("click", this._handleClickOnOverlay.bind(this));
     }
 
     _handleEscClose(evt) {
-        if (evt.key === 'Escape') {
+        if (evt.key === "Escape") {
             this.close();
         }
+    }
+
+    _handleClickOnOverlay(evt) {
+        if (evt.target !== this._popup) {
+            return;
+        }
+        this.close();
     }
 
     setEventListeners() {
         this._popup
             .querySelector(".popup__close-button")
             .addEventListener("click", this.close.bind(this));
-        document.addEventListener("keydown", this._handleEscClose.bind(this));
+        
     }
 }
